e9017f404dc5e9afaec3681cbb5695dbc4cab824
mechztcc <dojathan@gmail.com>
2021-09-25 14:29:45 -0300

    Fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewChecked, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'angular-restaurante';
 
-
+  session: any = localStorage.getItem('token');
 
   constructor() {}
+
+  ngDoCheck() {
+    this.session = localStorage.getItem('token');
+  
+  }
+
 
 }
--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnChanges {
+
+  session: boolean = false;
 
   constructor() { }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }     
   ngOnInit(): void {
-
+    
   }
 
 }
